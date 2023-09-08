8123295112a5846cc2cbfeb961463afdd7f839ce
jef-design <bermejojff97@gmail.com>
2023-09-08 17:55:41 +0800

    sdasd

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ import cors from "cors";
 
 const app = express();
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: ["https://mern-blog-frontend-theta.vercel.app"],
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
