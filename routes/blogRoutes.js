56f8ced47b59c26df78100d8eda45e5853106eba
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-08 22:13:52 +0800

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,11 +2,11 @@ import express from 'express'
 import { createBlog, deleteBlog, getAllBlogs, getBlog, updateBlog } from '../controllers/blogContollers.js'
 const router = express.Router()
 
-router.post('/create', createBlog)
-
 router.get('/', getAllBlogs)
 
 router.get('/:id', getBlog)
+
+router.post('/create', createBlog)
 
 router.delete('/:id', deleteBlog)
 
