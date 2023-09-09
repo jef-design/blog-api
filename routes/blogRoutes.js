518ac4af56edac8da392169e15c2cc922239a1ba
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-09 18:26:54 +0800

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import { getAllBlogs, getBlog, createBlog, deleteBlog, updateBlog } from '../controllers/blogContollers.js'
+const express = require('express')
+const { createBlog, deleteBlog, getAllBlogs, getBlog, updateBlog } = require('../controllers/blogContollers.js')
 const router = express.Router()
+
+router.post('/create', createBlog)
 
 router.get('/', getAllBlogs)
 
 router.get('/:id', getBlog)
-
-router.post('/create', createBlog)
 
 router.delete('/:id', deleteBlog)
 
@@ -14,4 +14,4 @@ router.patch('/:id', updateBlog)
 
 
 
-export default router
+module.exports = router
